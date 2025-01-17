42244e6ce52ea42e061144430dd078df50aa0158
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2025-01-17 16:23:41 -0500

    Add scores button

--- frontend/src/screens/RoundScreen.tsx
+++ frontend/src/screens/RoundScreen.tsx
@@ -8,6 +8,9 @@ import { Card } from '@/components/ui/card';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
 import { User } from '../types/user.types';
+import { HStack } from '@/components/ui/hstack';
+import { Avatar, AvatarFallbackText } from '@/components/ui/avatar';
+import { Button, ButtonText } from '@/components/ui/button';
 
 const RoundScreen: React.FC = () => {
 
@@ -18,6 +21,16 @@ const RoundScreen: React.FC = () => {
     const golfers = round.golfers.length < 4
     ? [...round.golfers, ...Array(4 - round.golfers.length).fill(null)]
     : round.golfers;
+
+    const getCurrentHole = (score: number[]) => {
+        for(let i = 0; i < score.length; i++) {
+            if(score[i] === null) {
+                return i + 1;
+            }
+        }
+        // To indicate round is over
+        return -1;
+    }
 
   return (
     <ScrollView>
@@ -30,19 +43,40 @@ const RoundScreen: React.FC = () => {
                 </Text>
             </Box>
             <Card className="p-5 mx-4 my-2">
-                <Heading>Scorecard</Heading>
+                <Heading>Current Hole</Heading>
+                <Box className="py-4 pr-4 flex flex-row justify-center items-center">
+                    <Text size="2xl" className="flex-1">{getCurrentHole(round.score)}</Text>
+                    <Button>
+                        <ButtonText>Add scores</ButtonText>
+                    </Button>
+                </Box>
             </Card>
             <Card className="p-5 mx-4 my-2">
                 <Heading>Golfers</Heading>
                 {golfers.map((golfer: User, index: number) => (
                     golfer === null ? (
-                        <Card className="bg-info-50 my-1">
-                            <Text>Golfer {index + 1}</Text>
+                        <Card key={index + 1} className="bg-info-50 my-1">
+                            <HStack space='md' className='items-center'>
+                                <Avatar>
+                                    <AvatarFallbackText>
+                                        Golfer
+                                    </AvatarFallbackText>
+                                </Avatar>
+                                
+                                <Text>Golfer {index + 1}</Text>
+                            </HStack>
                         </Card>
 
                     ) : (
-                        <Card>
-                            <Text>{golfer?.name}</Text>
+                        <Card key={golfer._id}>
+                            <HStack space='md' className='items-center'>
+                                <Avatar>
+                                    <AvatarFallbackText>
+                                        {golfer.name}
+                                    </AvatarFallbackText>
+                                </Avatar>
+                                <Text>{golfer.name}</Text>
+                            </HStack>
                         </Card>
                     )
                 ))}
