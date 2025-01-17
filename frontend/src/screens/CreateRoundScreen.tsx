1c5d8ae6acea87c8de73ec682b1fa27293368e7d
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2025-01-16 22:10:11 -0500

    Updates

--- frontend/src/screens/CreateRoundScreen.tsx
+++ frontend/src/screens/CreateRoundScreen.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import { Heading } from "@/components/ui/heading";
 import { Box } from "@/components/ui/box";
+import { Card } from "@/components/ui/card";
+import { Text } from "@/components/ui/text";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function CreateRoundScreen() {
+
+    const pressThatBih = () => {
+        alert("Yo! This bih was pressed homes!")
+    }
 
   return (
     <ScrollView
     >
-      <Box className="bg-primary-100 p-5">
-        <Heading>Welcome to Create Rounds on ZGolfApp!</Heading>
-      </Box>
+        <Box className="bg-primary-100 p-5">
+            <Heading>Welcome to Create Rounds on ZGolfApp!</Heading>
+        </Box>
+        <Card size="md" variant="elevated" className="bg-primary-50 m-3">
+            <Heading size="md" className="mb-1 pb-4">
+                Starting a round?
+            </Heading>
+            <Button size="md" variant="solid" action="primary" className="flex-1"
+                onPress={pressThatBih}>
+                <ButtonText>Create Match</ButtonText>
+            </Button>
+        </Card>
     </ScrollView>
   );
 }
